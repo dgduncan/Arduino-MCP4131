b61809b0ba9e5fd2afc565d1c93d8c9186c4e136
Derek Duncan <derekgduncan@gmail.com>
2018-04-03 22:49:48 -0500

    Created MCP header and cpp file

---
+++ src/MCP4131.h
@@ -0,0 +1,29 @@
+/*
+ 
+ 
+ Written by: Derek Duncan
+ Data: 04/03/2018
+ */
+
+#ifndef MCP4131_H
+#define MCP4131_H
+
+#include "Arduino.h"
+
+class MCP4131
+{
+    public:
+
+     // SegmentDisplay(int pin1, int pin2, int pin4, int pin5, int pin6, int pin7, int pin9, int pin10);
+     // void displayDecimalPoint();
+     // void displayHex(int numberToDisplay, boolean decimalPointFlag);
+     // void testDisplay();
+      // Not ready
+	  // void displaySaver();
+
+    private:
+	//
+    
+};
+
+#endif
