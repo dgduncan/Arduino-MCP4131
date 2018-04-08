88a94429e7059172a1272065e4602a2b38933ceb
Derek Duncan <derekgduncan@gmail.com>
2018-04-07 20:11:06 -0500

    Updated Keywords and src

--- src/MCP4131.cpp
+++ src/MCP4131.cpp
@@ -2,12 +2,39 @@
  Library written for use with Common-Anode 7-Segment Display. For more information on how to use this library,
  please refer to the github README.
  Written by: Derek Duncan
- Data: 2/4/2016
+ Data: 4/7/2018
 */
 
 #include "Arduino.h"
-#include "SegmentDisplay.h"
+#include "MCP4131.h"
+#include <SPI.h>
 
 MCP4131::MCP4131(int slave_select_pin) {
+    SPI.begin();
+    
 	pinMode(slave_select_pin, OUTPUT);
+}
+
+MCP4131::readWiper() {
+    SPI.beginTransaction(settingsA);
+    Serial.println("Entered Read");
+    // take the SS pin low to select the chip:
+    digitalWrite(slaveSelectPin, LOW);
+    //  send in the address and value via SPI:
+    byte error = SPI.transfer(0x0F);
+    byte result = SPI.transfer(0xFF);
+    //unsigned int result = SPI.transfer(0xC);
+    // take the SS pin high to de-select the chip:
+    digitalWrite(slaveSelectPin, HIGH);
+    SPI.endTransaction();
+    Serial.println(error);
+    Serial.println(result);
+    
+}
+
+MCP4131::checkIfError(byte error_byte) {
+
+	boolean error_bool = (error_byte & error_mask) << 1;
+
+	return error_bool;
 }
--- src/MCP4131.h
+++ src/MCP4131.h
@@ -9,13 +9,14 @@
 #define MCP4131_H
 
 #include "Arduino.h"
+#include <SPI.h>
 
 class MCP4131
 {
     public:
-	MCP4131(int slave_select_pin);
-	byte readWiper();
-	void writeWiper();
+        MCP4131(int slave_select_pin);
+        byte readWiper();
+        void writeWiper();
 
      // SegmentDisplay(int pin1, int pin2, int pin4, int pin5, int pin6, int pin7, int pin9, int pin10);
      // void displayDecimalPoint();
@@ -25,8 +26,9 @@ class MCP4131
 	  // void displaySaver();
 
     private:
-	boolean checkIfError();
-    
+        byte error_mask = 0x02;
+
+        boolean checkIfError(byte error_byte);
 };
 
 #endif
